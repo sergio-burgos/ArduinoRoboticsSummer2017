931884a4fb4072737a8ac3d045c46a22ad9709f4
sergio-burgos <sburgoscailloma@cca.edu>
2017-05-24 13:47:48 -0700

    Update README.ino

--- README.ino
+++ README.ino
@@ -1,8 +1,10 @@
 # ArduinoRoboticsSummer2017
 
 ```
-
-void setup() { // initialize digital pin 10 and 11 as an output. pinMode(10, OUTPUT); pinMode(11, OUTPUT); }
+void setup() { // initialize digital pin 10 and 11 as an output. 
+pinMode(10, OUTPUT); 
+pinMode(11, OUTPUT); 
+}
 
 // the loop function runs over and over again forever. I used a different delay time for each LED to easily identify pin 10 and 11 based on their behavior. 
 void loop() { digitalWrite(10, HIGH); // turn the LED 10 on (HIGH is the voltage level) 
@@ -15,3 +17,4 @@ delay(500); // wait for half a second
 digitalWrite(11, LOW); // turn the LED 11 off by making the voltage LOW 
 delay(500); // wait for half a second 
 }
+```
