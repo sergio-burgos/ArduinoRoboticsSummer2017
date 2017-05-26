b047ac46d8149d355955b9a7e3309a06eeb094b3
sergio-burgos <sburgoscailloma@cca.edu>
2017-05-26 11:19:25 -0700

    Create AnalogInput_On-Off.ino

---
+++ AnalogInput_On-Off.ino
@@ -0,0 +1,33 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  // open the serial port at 9600 bps:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read sensorValue as first step of loop:
+  Serial.println(sensorValue);
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  
+  // if the sensorValue is greater or equal to 14
+  if (sensorValue >= 14) {
+   // turn the ledPin on
+   digitalWrite(ledPin, HIGH);
+   // stop the program for <sensorValue> milliseconds:
+   delay(sensorValue);
+    // if the condition is false
+  
+  } else {
+   // turn the ledPin off:
+   digitalWrite(ledPin, LOW);
+   // stop the program for <sensorValue> milliseconds:
+   delay(sensorValue);
+  }
+
+}
