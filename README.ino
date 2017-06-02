450aa7c58eb5e6bce35057185677d3e546e68bfb
sergio-burgos <sburgoscailloma@cca.edu>
2017-06-02 09:36:19 -0700

    Update README.ino

--- README.ino
+++ README.ino
@@ -1,7 +1,6 @@
 # ArduinoRoboticsSummer2017
 
 ```
-
 void setup() { // initialize digital pin 10 and 11 as an output. 
 pinMode(10, OUTPUT); 
 pinMode(11, OUTPUT); 
@@ -18,7 +17,6 @@ delay(500); // wait for half a second
 digitalWrite(11, LOW); // turn the LED 11 off by making the voltage LOW 
 delay(500); // wait for half a second 
 }
-
 ```
 
 // 5/25 - Anlog Input Tutorial with LDR notes:
